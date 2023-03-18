2c5c4910218073947bc3518c7127ca539a571090
FackOff25 <kozvladimir@bk.ru>
2023-03-18 18:39:32 +0300

    blank lines in the end

--- scripts/perfTest.lua
+++ scripts/perfTest.lua
@@ -1,2 +1,2 @@
 -- Script for perfomance testing of server with 1000 requests, 20 parallel requests and maximum 1 second for response
-os.execute("ab -n 1000 -c 20 -s 1 127.0.0.1:80/httptest/wikipedia_russia.html")
+os.execute("ab -n 1000 -c 20 -s 1 127.0.0.1:80/httptest/wikipedia_russia.html");
