c585d2543d097a3165bb7a11d203fb899e5cde7a
FackOff25 <kozvladimir@bk.ru>
2023-03-18 18:34:55 +0300

    defining a serving folder | configs move | scripts commenting

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -2,7 +2,7 @@ FROM nginx
 
 WORKDIR /etc/nginx
 
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY config/nginx.conf /etc/nginx/nginx.conf
 COPY testing-module/httptest /etc/nginx/static/httptest
 
 EXPOSE 80
--- scripts/stopDocker.lua
+++ scripts/stopDocker.lua
@@ -1,2 +1,3 @@
--- Script for stoping Docker container with LuaServer
+-- Script for stoping Docker container with LuaServer.
 os.execute("sudo docker stop luaserver");
+-- This script exists definitely not because i am too lazy to write this comand, definitely not
